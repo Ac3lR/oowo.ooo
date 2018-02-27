c595fa4fc4e5870ff9c5b98165e9e810b132843c
Dylan <>
2018-02-27 17:46:57 +1100

    updates

--- include.js
+++ include.js
@@ -1,4 +1,5 @@
-<script>
+
+
 function includeHTML() {
   var z, i, elmnt, file, xhttp;
   /*loop through a collection of all HTML elements:*/
@@ -6,7 +7,7 @@ function includeHTML() {
   for (i = 0; i < z.length; i++) {
     elmnt = z[i];
     /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
+    file = elmnt.getAttribute("include");
     if (file) {
       /*make an HTTP request using the attribute value as the file name:*/
       xhttp = new XMLHttpRequest();
@@ -15,7 +16,7 @@ function includeHTML() {
           if (this.status == 200) {elmnt.innerHTML = this.responseText;}
           if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
           /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute("w3-include-html");
+          elmnt.removeAttribute("include");
           includeHTML();
         }
       } 
@@ -26,4 +27,3 @@ function includeHTML() {
     }
   }
 }
-</script>
