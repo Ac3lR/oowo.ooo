244cafc4aee38ea5e94017b8689451023b18d814
Dylan <>
2018-03-03 08:52:18 +1100

    Testing

--- links.js
+++ links.js
@@ -17,4 +17,6 @@ loadjscssfile("include.js", "js")
 loadjscssfile("https://use.fontawesome.com/releases/v5.0.7/js/all.js", "js") //dynamically load and add this .js file 
 loadjscssfile("https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.css", "css") ////dynamically load and add this .css file
 loadjscssfile("custom.css", "css")
+loadjscssfile("favicon.ico", "shortcut icon")
+
 
