76c7c7a271c09d07905c1a791bbc65a25eeb152f
Dylan <>
2018-03-07 19:35:21 +1100

    Something died

---
+++ test.py
@@ -0,0 +1,23 @@
+## for x in range(1, 101):
+    ## print x
+
+##print("1")
+##print("1")
+##x = 0
+
+##while x < 100:
+##    x = x + 1
+    ##print(x)
+
+
+## height = 10
+## height = height - 1
+
+## for x in range(1, height):
+    ## print(x * " " + "\\")
+    ## if x == height - 1:
+    ##    print((x+1) * "_" + "\\")
+n = "999"
+while n.length > 2:
+    a = n.split()
+    a[i]
