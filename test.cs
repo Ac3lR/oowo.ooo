2f82f6eeb9162690d7b25188cd3e8e47653cde67
Dylan <>
2018-04-15 20:06:40 +1000

    a

---
+++ test.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MyProgram {
+   class MyClass {
+      static void Main(string[] args) {
+        
+		bool condition = true;
+
+		if (condition) {
+		  Console.WriteLine("The variable is set to true.");
+		}
+		else {
+		  Console.WriteLine("The variable is set to false.");
+		} 
+      }
+    }
+}
