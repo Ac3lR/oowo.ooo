93438af59a26217092b56c369589bd8b0e14c3fd
Ac3lR <42797408+Ac3lR@users.noreply.github.com>
2019-02-22 21:04:06 +1100

    okokok
    fsfdfdf

--- code.js
+++
@@ -1,8 +0,0 @@
-setTimeout(function(){
-     hljs.initHighlightingOnLoad();
-
-     var all = document.getElementsByTagName("CODE")[0];
-      for (var i=0, max=all.length; i < max; i++) {
-          hljs.highlightBlock(i);
-      }
- }, 20);  
